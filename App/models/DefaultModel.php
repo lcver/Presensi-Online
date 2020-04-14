4ad56e82f0332dd9e6592ba6a1520f41bb066913
lcver <crackover1092@gmail.com>
2020-04-14 14:27:22 +0700

    set default model

---
+++ App/models/DefaultModel.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * 
+ * 
+ * call controller
+ */
+use App\Core\Controller;
+
+class DefaultModel extends Controller
+{
+    /**
+     * 
+     * create view resource data
+     */
+    public function create(){}
+        /**
+         * 
+         * stored new resourece data
+         */
+        public function store(){}
+            /**
+             * 
+             * display the specified resource data
+             */
+            public function show(){}
+                /**
+                 * 
+                 * display form for editing resource data
+                 */
+                public function edit($id){}
+                    /**
+                     * 
+                     * update the specified resource data
+                     */
+                    public function update($id){}
+                        /**
+                         * 
+                         * remove specified resource data
+                         */
+                        public function destroy($id){}
+}
