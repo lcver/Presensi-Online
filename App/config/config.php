e703cbbca5341c4a7926ab068ce90598b99c9e46
lcver <crackover1092@gmail.com>
2020-04-14 15:19:49 +0700

    set config

---
+++ App/config/config.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+// url
+define('BASEURL','http://localhost/project/Abdar/public/');
+define('BASEPATH',BASEURL.'../');
+
+// db
+define('DBHOST', 'localhost');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'db_elenka');
