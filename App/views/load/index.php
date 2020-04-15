93954393e19925259cfdbdcc2237e28db0c9038f
lcver <crackover1092@gmail.com>
2020-04-15 11:31:05 +0700

    delete load page

--- App/views/load/index.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <link rel="stylesheet" href="<?=BASEURL?>css/mainstyle.css">
-    <body>
-        <div class="bg-img-ppg">
-            <!-- <img src="<?=BASEURL?>img/logoPPG.jpeg" class="img-thumbnail" style="margin:auto; backgroun-color:red;"> -->
-        </div>
-    </body>
-</html>
