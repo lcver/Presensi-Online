631912783a15aac35ac9da82ea2fdbb8a9031cee
lcver <crackover1092@gmail.com>
2020-04-19 03:10:13 +0700

    modified jadwal view

--- App/views/admin/jadwal.php
+++ App/views/admin/jadwal.php
@@ -2,29 +2,74 @@
     <div class="col-md-12">
         <?=Flasher::get()?>
     </div>
-    <div class="row">
-        <?php if(isset($data['jadwal'])) : ?>
-        <?php foreach ($data['jadwal'] as $d) :?>
-        <div class="col-md-4">
-            <div class="card <?=$d['status']==2 ? 'bg-primary' : '';?>">
-                <div class="card-body">
-                    <button type="button" class="btn btn-tool float-right" onclick="btnAjax(<?=$d['id']?>,'<?=BASEURL?>admin/delete_jadwal')" data-card-widget="remove"><i class="fas fa-times"></i></button>
-                    <form action="<?=BASEURL?>admin/<?=$d['status']==2 ? 'inactive_jadwal' : 'activated_jadwal' ;?>" method="post">
-                        <input type="hidden" name="id" value="<?=$d['id']?>">
-                        Tanggal : <?=date_format(date_create($d['tanggal']),'D, d-m-Y')?>
-                        <?php if($d['status']==1) :?>
-                                <button type="submit" class="btn btn-primary btn-sm">Enable</button>
-                        <?php else: ?>
-                            <button class="btn btn-danger btn-sm">Disable</button>
-                        <?php endif; ?>
-                    </form>
+        <div class="card collapsed-card">
+            <div class="card-header bg-primary">
+                Daftar Jadwal
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
                 </div>
             </div>
+            <div class="card-body overflow-auto" style="max-height:200px;">
+                <?php if(isset($data['jadwal'])) : ?>
+                <?php foreach ($data['jadwal'] as $d) :?>
+                    <div class="card <?=$d['status']==2 ? 'bg-primary' : '';?>">
+                        <div class="card-body">
+                            <button type="button" class="btn btn-tool float-right" onclick="btnAjax(<?=$d['id']?>,'<?=BASEURL?>admin/delete_jadwal')" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                            <form action="<?=BASEURL?>admin/<?=$d['status']==2 ? 'inactive_jadwal' : 'activated_jadwal' ;?>" method="post">
+                                <input type="hidden" name="id" value="<?=$d['id']?>">
+                                Tanggal : <?=date_format(date_create($d['tanggal']),'D, d-m-Y')?>
+                                <?php if($d['status']==1) :?>
+                                        <button type="submit" class="btn btn-primary btn-sm float-right">Enable</button>
+                                <?php else: ?>
+                                    <button class="btn btn-danger btn-sm float-right">Disable</button>
+                                <?php endif; ?>
+                            </form>
+                        </div>
+                    </div>
+                <?php endforeach;?>
+                <?php endif;?>
+            </div>
         </div>
-        <?php endforeach;?>
-        <?php endif;?>
-    </div>
+        <!-- /.card -->
     <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Jadwal</h4>
+                </div>
+                <div class="card-body">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <form action="<?=BASEURL?>admin/set_jadwal" method="post">
+                                <!-- <ul class="list-unstyled m-0">
+                                    <li>
+                                        <span class="bg-primary pl-1 pr-1">Sesi 1 : 08.00 ~ 09.00</span>
+                                    </li>
+                                    <li>
+                                        <span class="bg-primary pl-1 pr-1">Sesi 2 : 09.30 ~ 11.00</span>
+                                    </li>
+                                    <li>
+                                        <span class="bg-primary pl-1 pr-1">Sesi 3 : 13.30 ~ 14.30</span>
+                                    </li>
+                                    <li>
+                                        <span class="bg-primary pl-1 pr-1">Sesi 4 : 20.00 ~ 21.30</span>
+                                    </li>
+                                </ul> -->
+                                <div class="form-group">
+                                    <label for="">Tanggal</label>
+                                    <input class="form-control d-inline dateform" type="date" name="presensi_tanggal" required>
+                                </div>
+                                <button class="btn btn-primary float-right">Set jadwal</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.card -->
+        </div>
+        <!-- /.col -->
+
         <div class="col-md-4">
             <div class="card">
                 <div class="card-header">
@@ -68,45 +113,10 @@
             </div>
         </div>
         <!-- /.col -->
-
-        <div class="col-md-4">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">jadwal</h4>
-                </div>
-                <div class="card-body">
-                    <div class="col-md-12">
-                        <div class="form-group">
-                            <form action="<?=BASEURL?>admin/set_jadwal" method="post">
-                                <!-- <ul class="list-unstyled m-0">
-                                    <li>
-                                        <span class="bg-primary pl-1 pr-1">Sesi 1 : 08.00 ~ 09.00</span>
-                                    </li>
-                                    <li>
-                                        <span class="bg-primary pl-1 pr-1">Sesi 2 : 09.30 ~ 11.00</span>
-                                    </li>
-                                    <li>
-                                        <span class="bg-primary pl-1 pr-1">Sesi 3 : 13.30 ~ 14.30</span>
-                                    </li>
-                                    <li>
-                                        <span class="bg-primary pl-1 pr-1">Sesi 4 : 20.00 ~ 21.30</span>
-                                    </li>
-                                </ul> -->
-                                <div class="form-group">
-                                    <label for="">Tanggal</label>
-                                    <input class="form-control d-inline dateform" type="date" name="presensi_tanggal" required>
-                                </div>
-                                <button class="btn btn-primary float-right">Set jadwal</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- /.card -->
-        </div>
-        <!-- /.col -->
     </div>
 </div>
+
+
 <div class="card">
     <div class="card-header">
         <h4 class="card-title">Presensi Asrama</h4>
