5f02d8eb56edd75025ff5edb261441aaf539c935
lcver <crackover1092@gmail.com>
2020-05-12 06:03:37 +0700

    Fix Basepath

--- App/config/config.php
+++ App/config/config.php
@@ -1,11 +1,17 @@
 <?php
 session_start();
 // url
-define('BASEURL','http://localhost/project/Abdar/public/');
-define('BASEPATH',BASEURL);
+define('BASEURL','http://localhost/Presensi-Online/public/');
+
+
+$app_path = preg_split('@/@', str_replace($_SERVER['DOCUMENT_ROOT'], '', realpath(dirname(__FILE__))), NULL, PREG_SPLIT_NO_EMPTY);
+$app_path = preg_replace('/config/', '', $app_path);
+$app_path = preg_replace('/App\\\/', '', $app_path);
+// define('APPPATH', $app_path[0]);
+define('BASEPATH',"/".$app_path[0]."/");
 
 // db
 define('DBHOST', 'localhost');
 define('DBUSER', 'root');
-define('DBPASS', '');
+define('DBPASS', 'Lucver1092');
 define('DBNAME', 'db_presensi_ppg');
