20e794794055e26d3ab83c1b2b295b8e8a3f00cb
lcver <crackover1092@gmail.com>
2020-05-29 13:16:15 +0700

    Fix display dashboard admin

--- App/views/admin/index.php
+++ App/views/admin/index.php
@@ -4,10 +4,11 @@
         <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
 
         <div class="info-box-content">
-        <span class="info-box-text">CPU Traffic</span>
+        <span class="info-box-text">Automatis Sistem</span>
         <span class="info-box-number">
-            10
-            <small>%</small>
+            click to 
+            <button class="btn btn-primary btn-sm no-border p-1">Enable</button>
+            
         </span>
         </div>
         <!-- /.info-box-content -->
@@ -17,11 +18,15 @@
     <!-- /.col -->
     <div class="col-12 col-sm-6 col-md-3">
     <div class="info-box mb-3">
-        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-calendar-alt"></i></span>
 
         <div class="info-box-content">
-        <span class="info-box-text">Likes</span>
-        <span class="info-box-number">41,410</span>
+        <span class="info-box-text">Jadwal</span>
+        <span class="info-box-number">
+            14/06/2020
+            <button class="btn btn-danger btn-sm no-border p-1">disable</button>
+            <!-- <span class="bg-primary p-1 text-bold rounded">auto</span> -->
+        </span>
         </div>
         <!-- /.info-box-content -->
     </div>
@@ -32,32 +37,22 @@
     <!-- fix for small devices only -->
     <div class="clearfix hidden-md-up"></div>
 
-    <div class="col-12 col-sm-6 col-md-3">
+    <div class="col-12 col-sm-10 col-md-4">
     <div class="info-box mb-3">
-        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-clock"></i></span>
 
         <div class="info-box-content">
-        <span class="info-box-text">Sales</span>
-        <span class="info-box-number">760</span>
+        <span class="info-box-text">Sesi</span>
+        <span class="info-box-number">
+            Asrama Al-qur'an
+            <button class="btn btn-danger btn-sm no-border p-1">disable</button>
+            <!-- <span class="bg-primary p-1 text-bold rounded">auto</span> -->
+        </span>
         </div>
         <!-- /.info-box-content -->
     </div>
     <!-- /.info-box -->
     </div>
-    <!-- /.col -->
-    <div class="col-12 col-sm-6 col-md-3">
-    <div class="info-box mb-3">
-        <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
-
-        <div class="info-box-content">
-        <span class="info-box-text">New Members</span>
-        <span class="info-box-number">2,000</span>
-        </div>
-        <!-- /.info-box-content -->
-    </div>
-    <!-- /.info-box -->
-    </div>
-    <!-- /.col -->
 </div>
 <!-- /.row -->
 
@@ -65,35 +60,20 @@
     <div class="col-md-12">
     <div class="card">
         <div class="card-header">
-        <h5 class="card-title">Monthly Recap Report</h5>
+        <h5 class="card-title">Rekap Laporan Presensi</h5>
 
         <div class="card-tools">
             <button type="button" class="btn btn-tool" data-card-widget="collapse">
             <i class="fas fa-minus"></i>
-            </button>
-            <div class="btn-group">
-            <button type="button" class="btn btn-tool dropdown-toggle" data-toggle="dropdown">
-                <i class="fas fa-wrench"></i>
-            </button>
-            <div class="dropdown-menu dropdown-menu-right" role="menu">
-                <a href="#" class="dropdown-item">Action</a>
-                <a href="#" class="dropdown-item">Another action</a>
-                <a href="#" class="dropdown-item">Something else here</a>
-                <a class="dropdown-divider"></a>
-                <a href="#" class="dropdown-item">Separated link</a>
-            </div>
-            </div>
-            <button type="button" class="btn btn-tool" data-card-widget="remove">
-            <i class="fas fa-times"></i>
             </button>
         </div>
         </div>
         <!-- /.card-header -->
         <div class="card-body">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-12">
             <p class="text-center">
-                <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
+                <strong>Abesensi : 15, Januari 2014 - sekarang</strong>
             </p>
 
             <div class="chart">
@@ -101,48 +81,6 @@
                 <canvas id="salesChart" height="180" style="height: 180px;"></canvas>
             </div>
             <!-- /.chart-responsive -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-4">
-            <p class="text-center">
-                <strong>Goal Completion</strong>
-            </p>
-
-            <div class="progress-group">
-                Add Products to Cart
-                <span class="float-right"><b>160</b>/200</span>
-                <div class="progress progress-sm">
-                <div class="progress-bar bg-primary" style="width: 80%"></div>
-                </div>
-            </div>
-            <!-- /.progress-group -->
-
-            <div class="progress-group">
-                Complete Purchase
-                <span class="float-right"><b>310</b>/400</span>
-                <div class="progress progress-sm">
-                <div class="progress-bar bg-danger" style="width: 75%"></div>
-                </div>
-            </div>
-
-            <!-- /.progress-group -->
-            <div class="progress-group">
-                <span class="progress-text">Visit Premium Page</span>
-                <span class="float-right"><b>480</b>/800</span>
-                <div class="progress progress-sm">
-                <div class="progress-bar bg-success" style="width: 60%"></div>
-                </div>
-            </div>
-
-            <!-- /.progress-group -->
-            <div class="progress-group">
-                Send Inquiries
-                <span class="float-right"><b>250</b>/500</span>
-                <div class="progress progress-sm">
-                <div class="progress-bar bg-warning" style="width: 50%"></div>
-                </div>
-            </div>
-            <!-- /.progress-group -->
             </div>
             <!-- /.col -->
         </div>
@@ -154,8 +92,8 @@
             <div class="col-sm-3 col-6">
             <div class="description-block border-right">
                 <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 17%</span>
-                <h5 class="description-header">$35,210.43</h5>
-                <span class="description-text">TOTAL REVENUE</span>
+                <h5 class="description-header">56</h5>
+                <span class="description-text">KEHADIRAN HARI INI</span>
             </div>
             <!-- /.description-block -->
             </div>
@@ -163,8 +101,8 @@
             <div class="col-sm-3 col-6">
             <div class="description-block border-right">
                 <span class="description-percentage text-warning"><i class="fas fa-caret-left"></i> 0%</span>
-                <h5 class="description-header">$10,390.90</h5>
-                <span class="description-text">TOTAL COST</span>
+                <h5 class="description-header">334</h5>
+                <span class="description-text">KEHADIRAN MINGGU INI</span>
             </div>
             <!-- /.description-block -->
             </div>
@@ -172,17 +110,17 @@
             <div class="col-sm-3 col-6">
             <div class="description-block border-right">
                 <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 20%</span>
-                <h5 class="description-header">$24,813.53</h5>
-                <span class="description-text">TOTAL PROFIT</span>
+                <h5 class="description-header">1324</h5>
+                <span class="description-text">KEHADIRAN BULAN INI</span>
             </div>
             <!-- /.description-block -->
             </div>
             <!-- /.col -->
             <div class="col-sm-3 col-6">
             <div class="description-block">
-                <span class="description-percentage text-danger"><i class="fas fa-caret-down"></i> 18%</span>
-                <h5 class="description-header">1200</h5>
-                <span class="description-text">GOAL COMPLETIONS</span>
+                <span class="description-percentage text-primary"><i class="fas fa-caret-down"></i></span>
+                <h5 class="description-header">3000</h5>
+                <span class="description-text">TOTAL KEHADIRAN</span>
             </div>
             <!-- /.description-block -->
             </div>
@@ -201,7 +139,7 @@
     <div class="col-md-8">
         <div class="card">
             <div class="card-header border-0">
-            <h3 class="card-title">Products</h3>
+            <h3 class="card-title">Laporan Pimpinan Anak Cabang</h3>
             <div class="card-tools">
                 <a href="#" class="btn btn-tool btn-sm">
                 <i class="fas fa-download"></i>
@@ -215,25 +153,23 @@
             <table class="table table-striped table-valign-middle">
                 <thead>
                 <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Sales</th>
+                <th>Nama</th>
+                <th>Total</th>
+                <th>Kehadiran</th>
                 <th>More</th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr>
                 <td>
-                    <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                    Some Product
+                    PAC Cempaka Baru
                 </td>
-                <td>$13 USD</td>
+                <td>15</td>
                 <td>
                     <small class="text-success mr-1">
                     <i class="fas fa-arrow-up"></i>
                     12%
                     </small>
-                    12,000 Sold
                 </td>
                 <td>
                     <a href="#" class="text-muted">
@@ -243,16 +179,14 @@
                 </tr>
                 <tr>
                 <td>
-                    <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                    Another Product
+                    PAC Podomoro
                 </td>
-                <td>$29 USD</td>
+                <td>49</td>
                 <td>
                     <small class="text-warning mr-1">
                     <i class="fas fa-arrow-down"></i>
-                    0.5%
+                    20%
                     </small>
-                    123,234 Sold
                 </td>
                 <td>
                     <a href="#" class="text-muted">
@@ -262,16 +196,14 @@
                 </tr>
                 <tr>
                 <td>
-                    <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                    Amazing Product
+                    PAC Johar Baru
                 </td>
-                <td>$1,230 USD</td>
+                <td>22</td>
                 <td>
                     <small class="text-danger mr-1">
                     <i class="fas fa-arrow-down"></i>
                     3%
                     </small>
-                    198 Sold
                 </td>
                 <td>
                     <a href="#" class="text-muted">
@@ -281,17 +213,14 @@
                 </tr>
                 <tr>
                 <td>
-                    <img src="dist/img/default-150x150.png" alt="Product 1" class="img-circle img-size-32 mr-2">
-                    Perfect Item
-                    <span class="badge bg-danger">NEW</span>
+                   PAC Menteng
                 </td>
-                <td>$199 USD</td>
+                <td>30</td>
                 <td>
                     <small class="text-success mr-1">
                     <i class="fas fa-arrow-up"></i>
                     63%
                     </small>
-                    87 Sold
                 </td>
                 <td>
                     <a href="#" class="text-muted">
